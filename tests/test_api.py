39674fda5f133cba7d7e05dfa62f909ff206148f
Connor Novak <connor@trilo.bio>
2024-11-12 12:30:27 -0800

    Add minimal unittests for gut check

---
+++ tests/test_api.py
@@ -0,0 +1,16 @@
+"""Unittests for tcode_api.api module."""
+import unittest
+
+from tcode_api.api import Fleet, TCodeAST
+
+
+class TestAPI(unittest.TestCase):
+
+    def test_tcodeast(self) -> None:
+        """Ensure that TCodeAST can be instantiated."""
+        ast = TCodeAST(fleet=Fleet(), tcode=[])
+        self.assertEqual(len(ast.tcode), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
