905a1a08623ba8aaae407bc57582e174391df628
Connor Novak <connor.novak@students.olin.edu>
2024-11-13 16:05:06 -0800

    TCode V1.2.0 - Deck Calibration
    * Create Location
    
    * Add Tools
      - Probe
    
    * Add TCode endpoints
      - CALIBRATE_FTS_NOISE_FLOOR
      - RESET_FTS
      - PROBE
    
    * Modify TCode endpoints
      - GOTO
    ---------
    
    Co-authored-by: Connor Novak <connor@trilo.bio>

--- tcode_api/api.py
+++ tcode_api/api.py
@@ -1,4 +1,6 @@
 """Pydantic BaseModel definitions of TCode API."""
+from __future__ import annotations
+
 from enum import Enum
 from typing import Annotated, List, Literal, Optional, Union
 
@@ -26,7 +28,50 @@ class TCODEBase(BaseModelStrict):
     pass
 
 
+class EnumWithDisplayName(Enum):
+    """Base class for Enum with display_name attribute."""
+
+    def __init__(self, value: int, display_name: str):
+        self._value_ = value
+        self.display_name = display_name
+
+    @classmethod
+    def from_value(cls, value: str | int) -> EnumWithDisplayName:
+        match type(value):
+            # Attempt to match by value
+            case int():
+                for member in cls:
+                    if member.value == value:
+                        return member
+
+            # Attempt to match by name or display_name
+            case str():
+                for member in cls:
+                    if member.name == value.upper() or member.display_name == value:
+                        return member
+
+            case _:
+                raise TypeError(f"Invalid value type: {type(value)}")
+
+
+Matrix = list[list[float]]
+
+
+class Location(BaseModelStrict):
+    """Resolvable location on a robot.
+
+    Three options:
+    - (int): 0-based index into a Fleet's labware list
+    - (str): name of a node in a robot's TransformTree
+    - (Matrix): transformation matrix relative to the robot's base
+    """
+    data: Union[int, str, Matrix]
+
+
 # Define constraint schemas
+class Probe(BaseModelStrict):
+    type: Literal["Probe"] = "Probe"
+
 class PipetteCommon(BaseModelStrict):
     min_volume: Optional[ValueWithUnits] = Field(default=None)
     max_volume: Optional[ValueWithUnits] = Field(default=None)
@@ -43,32 +88,32 @@ class EightChannelPipette(PipetteCommon):
 
 # Define the Tool discriminated union
 Tool = Annotated[
-    Union[SingleChannelPipette, EightChannelPipette],
+    Union[SingleChannelPipette, EightChannelPipette, Probe],
     Field(discriminator="type"),
 ]
 
 
-class GET_TOOL(TCODEBase):
-    type: Literal["GET_TOOL"] = "GET_TOOL"
-    tool: Tool
+class PathType(EnumWithDisplayName):
+    """Enumeration of robot path types.
 
-class DROP_TOOL(TCODEBase):
-    type: Literal["DROP_TOOL"] = "DROP_TOOL"
-
-
-class GET_TIP(TCODEBase):
-    type: Literal["GET_TIP"] = "GET_TIP"
-    location: int
-
-
-Matrix = list[list[float]]
+    DIRECT: robot moves to the target location directly in jointspace.
+    SAFE: robot moves to the target location via a safe path.
+    SHORTCUT: robot uses DIRECT if it is close to the target, othserwise SAFE.
+    """
+    DIRECT = (1, "Direct")
+    SAFE = (2, "Safe")
+    SHORTCUT = (3, "Shortcut")
 
 
-class GOTO(TCODEBase):
-    type: Literal["GOTO"] = "GOTO"
-    location: int
-    location_offset: Matrix
-    tool_offset: Matrix
+class TrajectoryType(EnumWithDisplayName):
+    """Enumeration of trajectory types.
+
+    JOINT_SQUARE: robot moves in joint space with square motor profiles.
+    JOINT_TRAPEZOIDAL: robot moves in joint space with trapezoidal motor profiles.
+    LINEAR: robot moves in cartesian space with non-uniform motor profiles.
+    """
+    JOINT_SQUARE = (1, "Square")
+    JOINT_TRAPEZOIDAL = (2, "Trapezoidal")
 
 
 class ASPIRATE(TCODEBase):
@@ -83,39 +128,76 @@ class DISPENSE(TCODEBase):
     speed: ValueWithUnits
 
 
+class CALIBRATE_FTS_NOISE_FLOOR(TCODEBase):
+    type: Literal["CALIBRATE_FTS_NOISE_FLOOR"] = "CALIBRATE_FTS_NOISE_FLOOR"
+    axes: str
+    snr: float
+
+
 class DROP_TIP(TCODEBase):
     type: Literal["DROP_TIP"] = "DROP_TIP"
-    location: int
+    location: Location
+
+
+class DROP_TOOL(TCODEBase):
+    type: Literal["DROP_TOOL"] = "DROP_TOOL"
+
+
+class GET_TIP(TCODEBase):
+    type: Literal["GET_TIP"] = "GET_TIP"
+    location: Location
+
+
+class GET_TOOL(TCODEBase):
+    type: Literal["GET_TOOL"] = "GET_TOOL"
+    tool: Tool
+
+
+class GOTO(TCODEBase):
+    type: Literal["GOTO"] = "GOTO"
+    location: Location
+    location_offset: Matrix
+    flange: Location
+    flange_offset: Matrix
+    path_type: PathType
+    trajectory_type: TrajectoryType
+
+
+class PROBE(TCODEBase):
+    type: Literal["PROBE"] = "PROBE"
+    location: Location
+    location_offset: Matrix
+    flange: Location
+    flange_offset: Matrix
+    speed_fraction: float
+    backoff_distance: ValueWithUnits
+
+
+class RESET_FTS(TCODEBase):
+    type: Literal["RESET_FTS"] = "RESET_FTS"
 
 
 TCODE = Annotated[
-    Union[GET_TOOL, GET_TIP, GOTO, ASPIRATE, DISPENSE, DROP_TIP, DROP_TOOL],
+    Union[
+        ASPIRATE,
+        CALIBRATE_FTS_NOISE_FLOOR,
+        DISPENSE,
+        DROP_TIP,
+        DROP_TOOL,
+        GET_TIP,
+        GET_TOOL,
+        GOTO,
+        PROBE,
+        RESET_FTS,
+    ],
     Field(discriminator="type"),
 ]
 
 
-class LabwareType(Enum):
+class LabwareType(EnumWithDisplayName):
     SAMPLE = (1, "Sample")
     TIP = (2, "Tip")
     TRASH = (3, "Trash")
-
-    def __init__(self, value, display_name):
-        self._value_ = value
-        self.display_name = display_name
-
-    @classmethod
-    def from_value(cls, value):
-        # Attempt to match by value
-        if isinstance(value, int):
-            for member in cls:
-                if member.value == value:
-                    return member
-        # Attempt to match by name or display_name
-        if isinstance(value, str):
-            for member in cls:
-                if member.name == value.upper() or member.display_name == value:
-                    return member
-        raise ValueError(f"'{value}' is not a valid LabwareType")
 
 
 class Labware(BaseModel):
