706886c791c9d398528d1caceb1ff383d5dff616
Connor Novak <connor@trilo.bio>
2025-03-20 14:21:14 -0700

    Create unittest to catch bug

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,39 @@
 import datetime
 import unittest
 
-from tcode_api.api import Fleet, Metadata, TCodeAST
+from tcode_api.api import EnumWithDisplayName, Fleet, Metadata, TCodeAST
+
+
+class TestEnumWithDisplayName(unittest.TestCase):
+    """EnumWithDisplayName unittests."""
+
+    class FruitType(EnumWithDisplayName):
+        """EnumWithDisplayName subclass, referenced in unittests."""
+
+        APPLE = (1, "Apple")
+        BANANA = (2, "Banana")
+        CHERRY = (3, "Cherry")
+
+    def test_from_value(self) -> None:
+        """Test from_value method on an EnumWithDisplayName subclass."""
+        with self.subTest("Good integer keys"):
+            self.assertEqual(self.FruitType.from_value(1), self.FruitType.APPLE)
+            self.assertEqual(self.FruitType.from_value(2), self.FruitType.BANANA)
+            self.assertEqual(self.FruitType.from_value(3), self.FruitType.CHERRY)
+
+        with self.subTest("Good string keys"):
+            self.assertEqual(self.FruitType.from_value("Apple"), self.FruitType.APPLE)
+            self.assertEqual(self.FruitType.from_value("Banana"), self.FruitType.BANANA)
+            self.assertEqual(self.FruitType.from_value("Cherry"), self.FruitType.CHERRY)
+
+        with self.subTest("Bad integer keys"):
+            self.assertRaises(ValueError, self.FruitType.from_value, 4)
+
+        with self.subTest("Bad string keys"):
+            self.assertRaises(ValueError, self.FruitType.from_value, "PEAR")
+
+        with self.subTest("Non-string and non-integer keys"):
+            self.assertRaises(TypeError, self.FruitType.from_value, [1])
 
 
 class TestAPI(unittest.TestCase):
