1ec9a43e5a8389138f0fcc8d997d20437c794ff5
Connor Novak <connor@trilo.bio>
2025-03-25 22:42:37 -0700

    Remove ellipses in error.py

--- src/tcode_api/error.py
+++ src/tcode_api/error.py
@@ -1,10 +1,6 @@
 class IdExistsError(ValueError):
     """Raised when a prior existing model with a specified id is found."""
 
-    ...
-
 
 class IdNotFoundError(ValueError):
     """Raised when a model with a specified id is not found."""
-
-    ...
