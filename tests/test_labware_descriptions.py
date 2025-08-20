0d0a58eb0f82b3d05bf857876178f27473ad4125
Connor Novak <connor@trilo.bio>
2025-08-20 16:00:46 -0700

    testing everything now

--- tests/test_labware_descriptions.py
+++ tests/test_labware_descriptions.py
@@ -13,8 +13,8 @@ labware_json_dir = pathlib.Path(__file__).parent.parent / "data"
 class TestLabwareDescriptions(unittest.TestCase):
     """Test that all labware descriptions are valid."""
 
-    def load_labware_description(self, filepath: pathlib.Path) -> None:
-        """Test that all labware descriptions are valid."""
+    def load_labware_descriptor(self, filepath: pathlib.Path) -> None:
+        """Test that labware description stored in file is a valid LabwareDescription."""
         with filepath.open("r") as f:
             content = f.read()
 
@@ -33,12 +33,33 @@ class TestLabwareDescriptions(unittest.TestCase):
 
         constructor(**content_json)
 
-    def test_labware_descriptions(self) -> None:
-        """Test that all labware descriptions are valid."""
+    def load_labware_description(self, filepath: pathlib.Path) -> None:
+        """Test that labware description stored in file is a valid LabwareDescriptor."""
+        with filepath.open("r") as f:
+            content = f.read()
+
+        content_json = json.loads(content)
+        match content_json["type"]:
+            case "WellPlate":
+                constructor = tc.WellPlateDescriptor
+            case "TipRack":
+                constructor = tc.TipRackDescriptor
+            case "Trash":
+                constructor = tc.TrashDescriptor
+            case "Lid":
+                constructor = tc.LidDescriptor
+            case _:
+                raise ValueError(f"Unknown labware type: {content_json['type']}")
+
+        constructor(**content_json)
+
+    def test_labware_files(self) -> None:
+        """Test that all labware json files are valid."""
         self.assertTrue(labware_json_dir.exists(), f"{labware_json_dir} does not exist")
         for file in labware_json_dir.iterdir():
             with self.subTest(file=file):
                 self.load_labware_description(file)
+                self.load_labware_descriptor(file)
 
 
 @plac.annotations(
@@ -48,6 +69,9 @@ def main(labware_filepath: pathlib.Path) -> None:
     """Run test_labware_descriptions for a single labware."""
     test_class = TestLabwareDescriptions()
     test_class.load_labware_description(labware_filepath)
+    print("Successfully loaded as LabwareDescription")
+    test_class.load_labware_descriptor(labware_filepath)
+    print("Successfully loaded as LabwareDescriptor")
 
 
 if __name__ == "__main__":
