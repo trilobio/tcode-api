369590cb79743227bb75930cae3ae232ed984b42
Connor Novak <connor@trilo.bio>
2025-09-29 07:50:32 -0700

    Re-organize submodules of tcode_api.api to improve documentation.

---
+++ src/tcode_api/api/core.py
@@ -0,0 +1,108 @@
+"""TCode API definitions, implemented with Pydantic."""
+
+from typing import Literal
+
+from pydantic import BaseModel, ConfigDict
+
+from tcode_api.types import Matrix
+
+
+class _ConfiguredBaseModel(BaseModel):
+    """pydantic.BaseModel with configuration to apply to all TCode data structures.
+
+    All TCode data structures should inherit from this class to ensure consistent
+    configuration.
+    """
+
+    model_config = ConfigDict(strict=True, extra="ignore")
+
+
+class _BaseModelWithId(_ConfiguredBaseModel):
+    """TCode-configured BaseModel with an ID field."""
+
+    id: str
+
+
+class ValueWithUnits(_ConfiguredBaseModel):
+    """A numeric value with associated units.
+
+    The following values are all equivalent:
+        ValueWithUnits(magnitude=0.005, units="L")
+        ValueWithUnits(magnitude=5, units="mL")
+        ValueWithUnits(magnitude=5000, units="uL")
+        ValueWithUnits(magnitude=5000, units="mm³")
+
+    In python, `pint` is used to resolve units.
+    """
+
+    type: Literal["ValueWithUnits"] = "ValueWithUnits"
+    magnitude: float
+    units: str
+
+    def __hash__(self):
+        # TODO (connor): Mia mentioned that this can produce collisions, fix.
+        return hash(self.magnitude) ^ hash(self.units)
+
+    def __str__(self):
+        return f"{self.magnitude} {self.units}"
+
+
+def identity_transform_factory() -> Matrix:
+    """Create new list of lists representing an identity matrix.
+
+    :return: 4x4 identity matrix as list of floats
+    """
+    return [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+
+
+def _verify_positive_nonzero_int(value: int) -> int:
+    """Validator to ensure the value is a positive non-zero integer.
+
+    :param value: The integer to validate.
+    :raises ValueError: If the value is not a positive non-zero integer.
+    :return: The validated integer.
+    """
+    if value <= 0:
+        raise ValueError(f"Value must be > 0, not {value}")
+
+    return value
+
+
+# Shapes
+class CircleDescription(_ConfiguredBaseModel):
+    """Description of a circle."""
+
+    type: Literal["Circle"] = "Circle"
+    diameter: ValueWithUnits
+
+
+class CircleDescriptor(_ConfiguredBaseModel):
+    """CircleDescription with optional parameters."""
+
+    type: Literal["Circle"] = "Circle"
+    diameter: ValueWithUnits | None = None
+
+
+class AxisAlignedRectangleDescription(_ConfiguredBaseModel):
+    """Description of an axis-aligned rectangle.
+
+    :note: X and Y lengths are measured in the coordinate system of the parent labware,
+        to avoid ambiguity when referring to the "length" or "width" of a rectangle.
+    """
+
+    type: Literal["AxisAlignedRectangle"] = "AxisAlignedRectangle"
+    x_length: ValueWithUnits
+    y_length: ValueWithUnits
+
+
+class AxisAlignedRectangleDescriptor(_ConfiguredBaseModel):
+    """AxisAlignedRectangleDescription with optional parameters."""
+
+    type: Literal["AxisAlignedRectangle"] = "AxisAlignedRectangle"
+    x_length: ValueWithUnits | None = None
+    y_length: ValueWithUnits | None = None
--- src/tcode_api/types.py
+++ src/tcode_api/types.py
@@ -5,4 +5,6 @@ NamedTags = dict[str, str | int | float | bool]
 
 UnsanitizedFloat = float | int | str
 
+Matrix = list[list[float]]
+
 CommandID = str  # Identifier specifically for TCode command
