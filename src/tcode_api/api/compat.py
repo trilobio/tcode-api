bd7d1535c0a8e0a00165652c0e004fa7f3f4ef0c
Connor Novak <connor@trilo.bio>
2026-03-03 09:02:16 -0800

    Add WIP compatibility layer to api; make api into full-fledged submodule

---
+++ src/tcode_api/api/compat.py
@@ -0,0 +1,322 @@
+"""Code to manage compatibiliy between different versions of tcode-api.
+
+Comtains:
+    * mapping from tcode-api semantic version (e.g. 'v1.35.1') to individual schema versions (e.g. SEND_WEBHOOK -> v2, WAIT -> v3)
+    * ``resolve_api_profile`` function to navigate the mapping.
+
+How to perform:
+    Rename:
+        * Rename the schema in the BASE_PROFILE mapping to the new name.
+        * Add an entry to API_RENAMES mapping the old name to the new name for the relevant APIVersion.
+
+    Deprecate:
+        * Remove the old command from the BASE_PROFILE mapping.
+        * Add an entry to API_CHANGELOG with the new APIVersion mapping the old command name to the new command name.
+
+    New:
+        * Add the new command to the BASE_PROFILE mapping with the appropriate version number.
+
+    REMOVE:
+        * Remove the command from the BASE_PROFILE mapping.
+        * Add an entry to API_REMOVALS with the removed command mapped to the APIVersion
+"""
+
+import dataclasses
+
+from packaging.version import Version
+
+APIVersion = str
+SchemaVersion = int
+SchemaName = str
+
+
+@dataclasses.dataclass
+class APIHistoryLog:
+    """Data structure to track a given APIs evolution history."""
+
+    name: str
+    """API name (e.g. "tcode-api"), used for logging purposes."""
+
+    increments: dict[APIVersion, dict[SchemaName, SchemaVersion]] = dataclasses.field(
+        default_factory=dict
+    )
+    """Mapping of API versions to version increments of schemas in that version.
+
+    Example:
+        {
+            "v0.1.0": {         # represents the initial supported release of the API
+                "POUR_TEA": 1,
+                "SMASH_CUP": 1,
+                "TeaCup": 1,
+            }
+            "v0.1.1": {
+                "POUR_TEA": 2,  # indicates that the ``POUR_TEA`` command was incremented to V2 in v0.1.1
+                "TeaCup": 2,    # indicates that the ``TeaCup`` schema was incremented to V2 in v0.1.1
+            },
+
+            # Other versions may have been released, but because they didn't change the API,
+            # they aren't represented in the changelog
+
+            "v0.2.0": {
+                "CoffeeCup": 1,  # indicates that the ``CoffeeCup`` schema was newly added in v0.2.0
+                "POUR_COFFEE": 1,# indicates that the ``POUR_COFFEE`` command was newly added in v0.2.0
+                "TeaCup": 3,     # indicates that the ``TeaCup`` schema was incremented to V3 in v0.2.0
+        }
+    """
+    renames: dict[APIVersion, dict[SchemaName, SchemaName]] = dataclasses.field(
+        default_factory=dict
+    )
+    """Mapping of API versions to mappings of old schema names to new schema names.
+
+    Example:
+        {
+            "v0.3.0": {
+                "POUR_TEA": "POUR",  # indicates that the ``POUR_TEA`` command was renamed to ``POUR`` in v0.2.0
+            }
+        }
+    """
+
+    replacements: dict[APIVersion, dict[SchemaName, SchemaName]] = dataclasses.field(
+        default_factory=dict
+    )
+    """Mapping of API versions to mappings of deprecated schema names to their replacements.
+
+    Example:
+        {
+            "v0.3.0": {
+                "POUR_COFFEE": "POUR",  # indicates that the ``POUR_COFFEE`` command was replaced
+            },                          # by the ``POUR`` command in v0.3.0
+        }
+    """
+
+    removals: dict[APIVersion, list[SchemaName]] = dataclasses.field(default_factory=dict)
+    """Mapping of API versions to lists of removed schema names.
+
+    Example:
+        {
+            "v0.4.0": [
+                "SMASH_CUP",  # indicates that, thankfully, the ``SMASH_CUP`` command was removed in v0.4.0
+            ]
+        }
+    """
+
+
+class TargetSchemaNotFoundError(Exception):
+    """Exception raised when a targeted schema is not found within in APIHistoryLog."""
+
+
+class TargetSchemaExistsError(Exception):
+    """Exception raised when a targeted schema already exists within in APIHistoryLog."""
+    schema_name: SchemaName
+
+
+class InvalidSchemaError(Exception):
+    """Exception raised on detection of an invalid schema.
+
+    Example cases include:
+        * Missing 'type' key
+        * 'schema_version' field doesn't match expected fields.
+    """
+    bad_schema: dict
+
+TCodeAPIHistory = APIHistoryLog(
+    name="tcode-api",
+    increments={
+        "v1.35.0": {
+            "ADD_LABWARE": 1,
+            "ADD_PIPETTE_TIP_GROUP": 1,
+            "ADD_ROBOT": 1,
+            "ADD_TOOL": 1,
+            "ASPIRATE": 1,
+            "CALIBRATE_LABWARE_HEIGHT": 1,
+            "CALIBRATE_LABWARE_WELL_DEPTH": 1,
+            "CALIBRATE_TOOL_FOR_PROBING": 1,
+            "COMMENT": 1,
+            "CREATE_LABWARE": 1,
+            "DELETE_LABWARE": 1,
+            "DISCARD_PIPETTE_TIP_GROUP": 1,
+            "DISPENSE": 1,
+            "MOVE_GRIPPER": 1,
+            "MOVE_TO_JOINT_POSE": 1,
+            "MOVE_TO_LOCATION": 1,
+            "PAUSE": 1,
+            "PICK_UP_LABWARE": 1,
+            "PICK_UP_PIPETTE_TIP": 1,
+            "PUT_DOWN_LABWARE": 1,
+            "PUT_DOWN_PIPETTE_TIP": 1,
+            "REMOVE_LABWARE_LID": 1,
+            "REPLACE_LABWARE_LID": 1,
+            "RETRIEVE_PIPETTE_TIP_GROUP": 1,
+            "RETRIEVE_TOOL": 1,
+            "RETURN_PIPETTE_TIP_GROUP": 1,
+            "RETURN_TOOL": 1,
+            "SEND_WEBHOOK": 1,
+            "SWAP_TO_TOOL": 1,
+            "WAIT": 1,
+            "Metadata": 1,
+            "TCode": 1,
+            "TCodeScript": 1,
+            "ValueWithUnits": 1,
+            "EightChannelPipetteDescriptor": 1,
+            "GripperDescriptor": 1,
+            "LabwareHolderDescriptor": 1,
+            "PipetteDescriptor": 1,
+            "ProbeDescriptor": 1,
+            "RobotDescriptor": 1,
+            "SingleChannelPipetteDescriptor": 1,
+            "ToolDescriptor": 1,
+            "ToolHolderDescriptor": 1,
+            "AxisAlignedRectangleDescription": 1,
+            "AxisAlignedRectangleDescriptor": 1,
+            "CircleDescription": 1,
+            "CircleDescriptor": 1,
+            "ConicalBottomDescription": 1,
+            "ConicalBottomDescriptor": 1,
+            "FlatBottomDescription": 1,
+            "FlatBottomDescriptor": 1,
+            "GridDescription": 1,
+            "GridDescriptor": 1,
+            "LidDescription": 1,
+            "LidDescriptor": 1,
+            "PipetteTipBoxDescription": 1,
+            "PipetteTipBoxDescriptor": 1,
+        },
+        "v1.36.0": {
+            "PICK_UP_LABWARE": 2,
+        },
+    },
+    renames={},
+    replacements={},
+    removals={},
+)
+
+
+def resolve_canonical_name(
+    api_version: APIVersion,
+    incoming_name: SchemaName,
+    api_history_log: APIHistoryLog = TCodeAPIHistory,
+) -> SchemaName:
+    """Resolve the canonical name given an incoming name an an API version - accounts for command
+    renames, deprecations, and removals.
+
+    :param api_version: The API version to resolve the name for.
+    :param incoming_name: The name to resolve.
+    :param api_history_log: The API history log to use for resolution. Defaults to the TCodeAPIHistory.
+        This parameter is primarily exposed for unittesting purposes.
+
+    :returns: the resolved name corresponding to ``api_version``.
+    """
+    requested = Version(api_version)
+    resolved_name = incoming_name
+
+    # 1. Renames
+    for v in sorted(api_history_log.renames, key=Version):
+        if Version(v) > requested:
+            break
+
+        if resolved_name in api_history_log.renames[v]:
+            resolved_name = api_history_log.renames[v][resolved_name]
+
+    # 2. Deprecations
+    for v in sorted(api_history_log.replacements, key=Version):
+        if Version(v) > requested:
+            break
+
+        if resolved_name in api_history_log.replacements[v]:
+            resolved_name = api_history_log.replacements[v][resolved_name]
+            # TODO (connor): emit deprecation warning
+
+    # 3. Removals
+    for v in sorted(api_history_log.removals, key=Version):
+        if Version(v) > requested:
+            break
+
+        if resolved_name in api_history_log.removals[v]:
+            raise ValueError(
+                f"{resolved_name} was removed in API version {v}, but was requested in version {api_version}."
+            )
+    return resolved_name
+
+
+def resolve_api_profile(
+    api_version: APIVersion,
+    api_history_log: APIHistoryLog = TCodeAPIHistory,
+) -> dict[SchemaName, SchemaVersion]:
+    """Resolve a schema-version profile for the given API version.
+
+    :param api_version: The API version to resolve the profile for.
+    :param api_history_log: The API history log to use for resolution. Defaults to the TCodeAPIHistory.
+        This parameter is primarily exposed for unittesting purposes.
+
+    :returns: a mapping of schema names to their respective versions for the given API version,
+        taking into account all changes up to that version.
+    """
+    requested = Version(api_version)
+
+    profile: dict[SchemaName, SchemaVersion] = {}
+
+    # Create sorted list of version strings that modified the API
+    versions_of_note = sorted(
+        set(api_history_log.increments.keys()).union(
+            set(api_history_log.renames.keys())).union(
+            set(api_history_log.replacements.keys())).union(
+            set(api_history_log.removals.keys())),
+        key=Version)
+    for version_str in versions_of_note:
+
+        # Check if we've got a modern enough version to return
+        if Version(version_str) > requested:
+            break
+
+        # Handle increments, removals
+        if version_str in api_history_log.increments:
+            for schema_name, schema_version in api_history_log.increments[version_str].items():
+                if schema_version is None:
+                    profile.pop(schema_name)
+                else:
+                    profile[schema_name] = schema_version
+
+        # Handle renames
+        if version_str in api_history_log.renames:
+            for old_schema_name, new_schema_name in api_history_log.renames[version_str].items():
+                if new_schema_name in profile:
+                    raise TargetSchemaExistsError(
+                        f"Cannot rename '{old_schema_name}' to '{new_schema_name}' in profile '{profile}' because '{new_schema_name}' already exists in the profile.")
+                try:
+                    profile[new_schema_name] = profile[old_schema_name]
+                except KeyError:
+                    raise TargetSchemaNotFoundError(
+                        f"'{old_schema_name}' not found in profile '{profile}'")
+                profile.pop(old_schema_name)
+
+        # Handle replacements
+        if version_str in api_history_log.replacements:
+            for old_schema_name in api_history_log.replacements[version_str]:
+                profile.pop(old_schema_name)
+
+
+    return profile
+
+
+def load_command(raw: dict, api_version: str) -> object:
+    try:
+        incoming_name = raw["type"]
+    except KeyError as err:
+        raise InvalidSchemaError(
+            msg="Unable to find expected key 'type' in command schema",
+            bad_schema=raw,
+        ) from err
+
+    canonical_name = resolve_canonical_name(api_version, incoming_name)
+    profile = resolve_api_profile(api_version)
+
+    try:
+        schema_version = profile[canonical_name]
+    except KeyError:
+        raise UnsupportedModelError(canonical_name, api_version)
+
+    return load_model(
+        model_name=canonical_name,
+        schema_version=schema_version,
+        raw=raw,
+    )
---
+++ tests/test_api/test_compat.py
@@ -0,0 +1,205 @@
+"""Unittests for the API migration and compatibility logic."""
+
+import unittest
+
+from tcode_api.api.compat import APIHistoryLog, resolve_api_profile, resolve_canonical_name, TargetSchemaNotFoundError, TargetSchemaExistsError
+
+TeaPartyAPIHistoryLog = APIHistoryLog(
+    name="tea-party-api",
+    increments={
+        "v0.1.0": {
+            "POUR_TEA": 1,
+            "SMASH_CUP": 1,
+            "TeaCup": 1,
+        },
+        "v0.1.1": {
+            "POUR_TEA": 2,
+            "TeaCup": 2,
+        },
+        "v0.2.0": {
+            "CoffeeCup": 1,
+            "POUR_COFFEE": 1,
+            "TeaCup": 3,
+        },
+        "v0.4.0": {
+            "SMASH_CUP": None,
+        }
+    },
+    renames={
+        "v0.3.0": {
+            "POUR_TEA": "POUR",
+        }
+    },
+    replacements={
+        "v0.3.0": {
+            "POUR_COFFEE": "POUR",
+        }
+    },
+)
+
+
+class TestResolveAPIProfile(unittest.TestCase):
+    """Tests for the ``resolve_api_profile`` function."""
+
+    def _assert_dicts_equal(self, dict1: dict, dict2: dict) -> None:
+        """Helper method to assert that two dicts are equal, ignoring order."""
+        missing_keys = set(dict1.keys()) - set(dict2.keys())
+        extra_keys = set(dict2.keys()) - set(dict1.keys())
+        bad_values = {key: (dict1[key], dict2[key]) for key in dict1 if key in dict2 and dict1[key] != dict2[key]}
+        if len(missing_keys) > 0 or len(extra_keys) > 0 or len(bad_values) > 0:
+            error_message = (
+                "Dicts are not equal:\n"
+                "  Dict1: {dict1}\n"
+                "  Dict2: {dict2}\n"
+                ).format(dict1=dict1, dict2=dict2)
+            if len(missing_keys) > 0:
+                error_message += f"  dict2 missing keys: {missing_keys}\n"
+            if len(extra_keys) > 0:
+                error_message += f"  dict2 has extra keys: {extra_keys}\n"
+            if len(bad_values) > 0:
+                for key, (value1, value2) in bad_values.items():
+                    error_message += f"  Key '{key}' has different values: dict1={value1}, dict2={value2}\n"
+            self.fail(error_message)
+
+    def test_replacement_without_increments(self) -> None:
+        """Test that a version that had a replacement without any increments still resolves correctly."""
+        api_history_log = APIHistoryLog(
+            name="",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "C": 2},
+            },
+            replacements={
+                "v0.1.1": {"B": "A"},
+            },
+        )
+
+        self._assert_dicts_equal(
+            {"A": 2, "C": 2},
+            resolve_api_profile("v0.2.0", api_history_log)
+        )
+
+    def test_rename_without_increment(self) -> None:
+        """A version with a rename and no increment should resolve correctly."""
+        api_history_log = APIHistoryLog(
+            name="test_rename_without_increment",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "C": 2},
+            },
+            renames={
+                "v0.1.1": {"B": "D"},
+            },
+        )
+
+        self._assert_dicts_equal(
+            {"A": 2, "C": 2, "D": 1},
+            resolve_api_profile("v0.2.0", api_history_log)
+        )
+
+    def test_increment_after_rename(self) -> None:
+        """A schema renamed in a prior version should increment from the pre-renamed version."""
+        api_history_log = APIHistoryLog(
+            name="test_increment_after_rename",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "C": 2},
+                "v0.3.0": {"D": 2},
+            },
+            renames={
+                "v0.2.0": {"B": "D"},
+            },
+        )
+
+        self._assert_dicts_equal(
+            {"A": 2, "C": 2, "D": 2},
+            resolve_api_profile("v0.3.0", api_history_log)
+        )
+
+    def test_increment_and_rename(self) -> None:
+        """A schema that is incremented and renamed in a single version should show the increment with the new name."""
+        api_history_log = APIHistoryLog(
+            name="test_increment_after_rename",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "B": 2, "C": 2},
+            },
+            renames={
+                "v0.2.0": {"B": "D"},
+            },
+        )
+
+        self._assert_dicts_equal(
+            {"A": 2, "D": 2, "C": 2},
+            resolve_api_profile("v0.2.0", api_history_log)
+        )
+
+    def test_rename_nonexistent(self) -> None:
+        """A rename with a non-existent target raises an error."""
+        api_history_log = APIHistoryLog(
+            name="test_increment_after_rename",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "B": 2, "C": 2},
+            },
+            renames={
+                "v0.2.0": {"D": "E"},
+            },
+        )
+
+        with self.assertRaises(TargetSchemaNotFoundError):
+            resolve_api_profile("v0.2.0", api_history_log)
+
+    def test_rename_to_existing(self) -> None:
+        """A rename to an existing target raises an error."""
+        api_history_log = APIHistoryLog(
+            name="test_increment_after_rename",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"C": 2},
+            },
+            renames={
+                "v0.2.0": {"B": "A"},
+            },
+        )
+
+        with self.assertRaises(TargetSchemaExistsError):
+            resolve_api_profile("v0.2.0", api_history_log)
+
+    def test_too_early_version(self) -> None:
+        """Requesting a version before the first increment should return an empty profile."""
+        api_history_log = APIHistoryLog(
+            name="test_too_early_version",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+            },
+        )
+        self._assert_dicts_equal(
+            {},
+            resolve_api_profile("v0.0.1", api_history_log),
+        )
+
+    def test_implied_version(self) -> None:
+        """Requesting a version that doesn't exist should resolve to the latest version before it."""
+        api_history_log = APIHistoryLog(
+            name="test_nonexistent_version",
+            increments={
+                "v0.1.0": {"A": 1, "B": 1, "C": 1},
+                "v0.2.0": {"A": 2, "C": 2},
+            },
+        )
+        self._assert_dicts_equal(
+            {"A": 1, "B": 1, "C": 1},
+            resolve_api_profile("v0.1.5", api_history_log),
+        )
+        self._assert_dicts_equal(
+            {"A": 2, "B": 1, "C": 2},
+            resolve_api_profile("v0.2.5", api_history_log),
+        )
+
+    def test_invalid_version(self) -> None:
+        """Test that an error is raised when an invalid version string is specified."""
+        bad_versions = ["invalid_version", "version_0.1.0"]
+        for bad_version in bad_versions:
+            with self.subTest(bad_version=bad_version), self.assertRaises(ValueError):
+                resolve_api_profile(bad_version, TeaPartyAPIHistoryLog)
